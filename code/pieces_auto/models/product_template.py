aae69ff582f3fe262511ede27dc83707c6ccfc49
isaac.r <isaac1hcw@gmail.com>
2022-10-11 16:21:26 +0100

    main

--- code/pieces_auto/models/product_template.py
+++ code/pieces_auto/models/product_template.py
@@ -6,13 +6,27 @@ class ProductTemplate(models.Model):
 
     _inherit = "product.template"
 
+    name = fields.Char(
+        string='Product name',
+        required=True, 
+        translate=True,
+    )
+
     mark_id = fields.Many2one(
         comodel_name='mark.auto',
         )
-    original_reference = fields.Char()
+    reference_oe = fields.Char()
+
+    short_reference = fields.Char()
 
     comp_ids = fields.Many2many(
-        comodel_name='product.comp'
+        comodel_name='product.comp'        
     )
+
+
+    designation = fields.Char()
+
+
+
 
 
